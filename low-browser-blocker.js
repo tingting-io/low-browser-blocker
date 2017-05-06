0e66cd28b3430f979fb04d1c65a216ceb03a065e
JingAn Chen <lzsoft.cja@gmail.com>
2017-05-05 21:12:04 -0400

    Update.

--- low-browser-blocker.js
+++ low-browser-blocker.js
@@ -2,16 +2,25 @@
     if (window.fetch && window.customElements && 'import' in document.createElement('link') && 'content' in document.createElement('template')) {
         //
     } else {
+        var div = document.createElement("div");
+        var div2 = document.createElement("div");
         var td = document.createElement("td");
         var tr = document.createElement("tr");
         var table = document.createElement("table");
         td.style.verticalAlign = "middle";
         td.style.textAlign = "center";
-        td.innerHTML = "Your browser is not supported by our website";
-        tr.append(td);
-        table.append(tr);
+        div.style.fontSize = "36px";
+        div2.style.fontSize = "24px";
+        div2.style.marginTop = "20px";
+        div.innerHTML = "Your browser is not supported by our website";
+        div2.innerHTML = "Please use the latest Chrome, Safari, Firefox or Edge to visit us";
+        td.appendChild(div);
+        td.appendChild(div2);
+        tr.appendChild(td);
+        table.appendChild(tr);
         table.style.height = '100%';
         table.style.width = '100%';
-        document.body.append(table);
+        document.body.innerHTML = "";
+        document.body.appendChild(table);
     }
 })();
