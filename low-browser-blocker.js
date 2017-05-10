c523b309daf1fbdf14523835aeee860a3355b445
JingAn Chen <lzsoft.cja@gmail.com>
2017-05-09 20:55:36 -0400

    Fuck IE11.

--- low-browser-blocker.js
+++ low-browser-blocker.js
@@ -24,11 +24,11 @@
         div5.style.fontSize = "16px";
         div5.style.marginTop = "30px";
         div5.style.color = "#999";
-        div.innerHTML = `Your browser is not supported by our website`;
-        div2.innerHTML = `Please use the latest <a style="display: inline; text-decoration: underline; color: blue;" href="https://www.google.com/chrome/browser/desktop/">Chrome</a> or Safari to visit us`;
-        div3.innerHTML = `The reason we don't support your browser is that we are a small startup company`;
-        div4.innerHTML = `and we have to invest our very limited development resource into the best browsers`;
-        div5.innerHTML = `and yes, believe me or not, there is a huge difference between browsers`;
+        div.innerHTML = 'Your browser is not supported by our website';
+        div2.innerHTML = 'Please use the latest <a style="display: inline; text-decoration: underline; color: blue;" href="https://www.google.com/chrome/browser/desktop/">Chrome</a> or Safari to visit us';
+        div3.innerHTML = 'The reason we don\'t support your browser is that we are a small startup company';
+        div4.innerHTML = 'and we have to invest our very limited development resource into the best browsers';
+        div5.innerHTML = 'and yes, believe me or not, there is a huge difference between browsers';
         td.appendChild(div);
         td.appendChild(div2);
         td.appendChild(div3);
